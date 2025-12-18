53480af8a1ba4a120c4ef0eb07bf1e9998f02982
ololukaszuk <47779810+ololukaszuk@users.noreply.github.com>
2025-12-18 01:46:34 +0100

    `Update Vite configuration to use esbuild minifier`

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -17,11 +17,6 @@ export default defineConfig({
     outDir: 'dist',
     assetsDir: 'assets',
     sourcemap: false,
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-      },
-    },
+    minify: 'esbuild',
   }
 })
